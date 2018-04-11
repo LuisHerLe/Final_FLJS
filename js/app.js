ea886198c5527a7f8af211f4d479daf062415b3e
Luis Hernandez <luifo2033@gmail.com>
2018-04-11 16:16:37 -0500

    Se agrega codificación para movimiento, conteo y eliminar imágenes

--- js/app.js
+++ js/app.js
@@ -44,15 +44,7 @@ $(".btn-reinicio").click(function(){
       }
     );
 
-    findHorizontal()
-    //moveCandie();
-    //var options = $( "img" ).draggable( "option" );
-    //console.log(options);
-
-    //$( "img" ).draggable({
-    //revert: true
-    //}); //Regresar la posición de la imagen!!!!
-
+eliminarhorver()
     // NOTE: Punto 7: Se habilita el movimiento de las fichas
   }else {
     location.reload();
@@ -167,4 +159,136 @@ function findMatch(){
       }
     }
   }
+}
+
+
+
+// NOTE: ***************************************************
+function eliminarhorver()
+{
+  matriz=0;
+  rbh=horizontal()  //funcion busqueda dulces horizontal
+  rbv=vertical()    //funcion buscar dulces vertical
+
+  for(var j=1;j<8;j++)
+  {
+      matriz=matriz+$(".col-"+j).children().length;
+  }
+
+  if(rbh==0 && rbv==0 && matriz!=49)  //condicion si no encuentra 3 dulces o mas llamar a funcion para volver a completar el uego
+  {
+      clearInterval(eliminar);
+      bnewd=0;
+      newdulces=setInterval(function()
+      {
+        nuevosdulces()  //Funcion completar nuevos dulces
+      },600)
+  }
+  if(rbh==1 || rbv==1)
+  {
+    $(".elemento").draggable({ disabled: true });
+    $("div[class^='col']").css("justify-content","flex-end")
+    $(".activo").hide("pulsate",1000,function(){
+      var scoretmp=$(".activo").length;
+      $(".activo").remove("img")
+      score=score+scoretmp;
+      $("#score-text").html(score)  //Cambiar puntuacion
+    })
+  }
+
+  if(rbh==0 && rbv==0 && matriz==49)
+  {
+    $(".elemento").draggable({
+      disabled: false,
+      containment: ".panel-tablero",
+      revert: true,
+      revertDuration: 0,
+      snap: ".elemento",
+      snapMode: "inner",
+      snapTolerance: 40,
+      start: function(event, ui){
+        mov=mov+1;
+        $("#movimientos-text").html(mov)
+      }
+    });
+  }
+
+  $(".elemento").droppable({
+    drop: function (event, ui) {
+      var dropped = ui.draggable;
+      var droppedOn = this;
+      espera=0;
+      do{
+        espera=dropped.swap($(droppedOn));
+      }while(espera==0)
+      rbh=horizontal()  //funcion busqueda dulces horizontal
+      rbv=vertical()    //funcion buscar dulces vertical
+      if(rbh==0 && rbv==0)
+      {
+        dropped.swap($(droppedOn));
+      }
+      if(rbh==1 || rbv==1)
+      {
+        clearInterval(newdulces);
+        clearInterval(eliminar);   //desactivar funcion desplazamiento()
+        eliminar=setInterval(function(){eliminarhorver()},150)  //activar funcion eliminarhorver
+      }
+    },
+  });
+}
+//------------------------------------------------------------------------------
+//---------Funcion para intercambiar dulces-------------------------------------
+jQuery.fn.swap = function(b)
+{
+    b = jQuery(b)[0];
+    var a = this[0];
+    var t = a.parentNode.insertBefore(document.createTextNode(''), a);
+    b.parentNode.insertBefore(a, b);
+    t.parentNode.insertBefore(b, t);
+    t.parentNode.removeChild(t);
+    return this;
+};
+	function horizontal()
+{
+  var bh=0;
+  for(var j=1;j<8;j++)
+  {
+    for(var k=1;k<6;k++)
+    {
+      var res1=$(".col-"+k).children("img:nth-last-child("+j+")").attr("src")
+      var res2=$(".col-"+(k+1)).children("img:nth-last-child("+j+")").attr("src")
+      var res3=$(".col-"+(k+2)).children("img:nth-last-child("+j+")").attr("src")
+      if((res1==res2) && (res2==res3) && (res1!=null) && (res2!=null) && (res3!=null))
+      {
+          $(".col-"+k).children("img:nth-last-child("+(j)+")").attr("class","elemento activo")
+          $(".col-"+(k+1)).children("img:nth-last-child("+(j)+")").attr("class","elemento activo")
+          $(".col-"+(k+2)).children("img:nth-last-child("+(j)+")").attr("class","elemento activo")
+          bh=1;
+      }
+    }
+  }
+  return bh;
+}
+//------------------------------------------------------------------------------
+//----------Funcion de busqueda vertical de dulces------------------------------
+function vertical()
+{
+  var bv=0;
+  for(var l=1;l<6;l++)
+  {
+    for(var k=1;k<8;k++)
+    {
+      var res1=$(".col-"+k).children("img:nth-child("+l+")").attr("src")
+      var res2=$(".col-"+k).children("img:nth-child("+(l+1)+")").attr("src")
+      var res3=$(".col-"+k).children("img:nth-child("+(l+2)+")").attr("src")
+      if((res1==res2) && (res2==res3) && (res1!=null) && (res2!=null) && (res3!=null))
+      {
+          $(".col-"+k).children("img:nth-child("+(l)+")").attr("class","elemento activo")
+          $(".col-"+k).children("img:nth-child("+(l+1)+")").attr("class","elemento activo")
+          $(".col-"+k).children("img:nth-child("+(l+2)+")").attr("class","elemento activo")
+          bv=1;
+      }
+    }
+  }
+  return bv;
 }
