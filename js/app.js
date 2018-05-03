0e9932136fe4804cc5a7cde4b4eae8d5be5f02f1
Luis Hernandez <luifo2033@gmail.com>
2018-05-03 15:03:36 -0500

    Se corrige el movimiento y el intercambio de fichas

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-
 $(document).ready(function(){
 
   var start = false;
+   $(".timer").html("02:00");
   // NOTE: Punto 1: Animación del título Match Game #DCFF0E
   setInterval(function(){
     $(".main-titulo").stop().animate({color:'white'},1000);
@@ -9,21 +9,41 @@ $(document).ready(function(){
   },1800)
 
   // NOTE: Punto 2: Se generan las imágenes de forma aleatoria
-  setInterval(getRowCandies(),1000);
+  //setInterval(getRowCandies(),1000);
 
 })
-// ********************** INICIO VARIABLES GLOBALES **********************
 
-var countMov=0;
-var listDelete = new Array();
-var findedH = "";
-var findedV = "";
-var aux = "";
-var mov = "";
-// ********************** FIN VARIABLES GLOBALES **********************
+var rbh=0;
+var rbv=0;
+var bnewd=0;
+var lencol=["","","","","","",""];
+var lenres=["","","","","","",""];
+var maximo=0;
+var arrayCandies=0;
 
-// NOTE:Punto 6. Se inicia juego o se reinicia contenido al dar click al botón cuando este diga "Reiniciar"
+var intervalo=0;  //variable de tiempo para funcion de desplazamiento
+var eliminar=0;   //variable de tiempo para eliminar dulces
+var newdulces=0;  //variable de tiempo para nuevos dulces
+var tiempo=0;     //variable de tiempo para temporizador
+
+var i=0;
+var contador=0;  //contador total
+var conc1=0;    //contador columna1
+
+var initialPos;
+var espera=0;
+var score=0;
+var mov=0;
+
+var min=2;
+var seg=0;
+
+    var counter = 0;
+    var timeLeft = 120; //Está en segundos
+
+
 $(".btn-reinicio").click(function(){
+
   var contButton = $(".btn-reinicio").html();
   start = true;
   if (contButton == "Iniciar") {
@@ -31,128 +51,53 @@ $(".btn-reinicio").click(function(){
 
     if (start) {
       // NOTE: Se llama inicia el temporizador
-      setup(start);
+	  setup(start)
     }
-
-    $("img").draggable(
-      {
-        disabled: false,
-        containment: ".panel-tablero",
-        revert: true,
-        revertDuration: 1,
-        snap: ".elemento",
-        snapMode: "inner",
-        snapTolerance: 40,
-      }
-    );
-
-
     // NOTE: Punto 7: Se habilita el movimiento de las fichas
     // NOTE: Acá va la lógica
   }else {
     location.reload();
   }
+
+
+//inicio nextu
+  i=0;
+  score=0;
+  mov=0;
+  $(".panel-score").css("width","25%");
+
+  clearInterval(intervalo);
+  clearInterval(eliminar);
+  clearInterval(newdulces);
+  clearInterval(tiempo);
+  min=2;  //2
+  seg=0;  //0
+  borrartotal()
+  intervalo=setInterval(function(){desplazamiento()},600)
+
+  //fin nextu
 })
 
-// NOTE: Punto 2: Se generan las imágenes de forma aleatoria, primero se crea la estructura dentro del DOM
-function getRowCandies(){
-  var colName = "";
-  var auxCol = "";
-  var auxRow = "";
-  var auxChildren = "";
-
-  //Se recorren las columnas
-  for (var i = 1; i < 8; i++) {
-    colName = ".panel-tablero .col-"+i
-    auxCol = ".col-"+i
-
-    //Se recorren las filas
-    for (var r = 1; r < 8; r++) {
-      auxRow = ".row-"+r
-      auxChildren = ".col-"+i+ "> .row-"+r
-
-      if ($(auxChildren).children().length == 0) { //Saber si el campo está vacío
-        //console.log("Vacío col: " + auxCol + " con fila: " + auxRow);
-        var rowNew = $("<div class=row-"+r+"><img id= class-"+i+"row-"+r+" src='' alt=''></div>")
-        rowNew.appendTo(colName);
-        getImgCandies(rowNew);
-      }
-    }
-  }
-  if (i == 8 && r ==8) {
-
-    //setInterval(function(){findHorizontal()},150);
-    //  setInterval(function(){findVertical()},150);
-    findVertical(listDelete);
-    findHorizontal(listDelete);
-
-  }
-}
-
-function getImgCandies(rowNew){
-  // TODO: CAMBIAR!!!!!!!!!
-  var candyRandom = Math.round(Math.random()*4);
-  //var candyRandom = 1; //Para pruebas
-  while (candyRandom == 0) {
-    candyRandom = Math.round(Math.random()*4)
-  }
-  var newCandy = "image/"+candyRandom+".png"
-  rowNew.find("img").attr("src",newCandy);
-}
-
-// NOTE: Punto 4: Función para temporizador usando p5
+// NOTE: Función para temporizador usando p5
 function setup(start){
   if (start) {
-    var counter = 0;
-    var timeLeft = 120; //Está en segundos
+	  setInterval(countDown, 1000);
+ }
 
-    // TODO: OJO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    $("img").draggable({
-    disabled: false,
-    containment: ".panel-tablero",
-    revert: true,
-    revertDuration: 0,
-    snap: ".elemento",
-    snapMode: "inner",
-    snapTolerance: 40,
-    start: function(event, ui){
-      mov=mov+1;
-      $("#movimientos-text").html(mov)
+    //noCanvas();
+    var timer = $("#timer");
+    timer.html(convertMinutes(timeLeft - counter))
+    function countDown(){
+      counter ++;
+      timer.html(convertMinutes(timeLeft - counter))
     }
-  });
-}
 
-$("img").droppable({
-  drop: function (event, ui) {
-    var dropped = ui.draggable;
-    var droppedOn = this;
-    espera=0;
-    do{
-      espera=dropped.swap($(droppedOn));
-    }while(espera==0)
-    fingvertical()    //funcion buscar dulces vertical
-    findHorizontal()  //funcion busqueda dulces horizontal
-
-    if(listDelete.length != 0)
-    {
-      dropped.swap($(droppedOn));
-    }else
-    {
-      clearInterval(newdulces);
-      clearInterval(eliminar);   //desactivar funcion desplazamiento()
-      eliminar=setInterval(function(){eliminarhorver()},150)  //activar funcion eliminarhorver
-    }
-  },
-});
-
-    //OJO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    //Convertir a minutos + segundos
+	    //Convertir a minutos + segundos
     function convertMinutes(s){
       var min = floor (s / 60);
-      var sec = s % 60;
+      var sec = floor (s % 60);
       var time = nf(min,2) + ":" + nf(sec,2);
+	  //var time = "02:00"
       if (min == 0 && sec == 0) {
         //NOTE: Punto 4: Se termina el juego
         endGame();
@@ -160,19 +105,10 @@ $("img").droppable({
       return time;
 
     }
-
-    noCanvas();
-    var timer = $("#timer");
-    timer.html(convertMinutes(timeLeft - counter))
-    function countDown(){
-      counter ++;
-      timer.html(convertMinutes(timeLeft - counter))
-    }
-    setInterval(countDown, 1000);
   }
 
-
-function endGame(){
+  // NOTE: Función para terminar el juego
+  function endGame(){
   // NOTE: Punto 4: Se oculta el tablero de juego y el tiempo, adicional se agranda al 100% el ancho del panel de puntos
   $(".time").hide(1000,"swing");
   $(".panel-tablero").hide(1000,"swing", function(){
@@ -185,168 +121,231 @@ function endGame(){
   rowEnd.appendTo(gameName);
 }
 
-// NOTE: Buscar igualdades para mandar eliminar ****EN CONSTRUCCIÓN****
-function findVertical(listDelete){
+//----------Funcion de borrado--------------------------------------------------
+function borrartotal()
+{
+  for(var i=1;i<8;i++)
+  {
+    $(".col-"+i).children("img").detach();
+  }
+}
+//---------------Funcion inicial para llenar el cuadro del juego----------------
+function desplazamiento()
+{
+  i=i+1
+  var numero=0;
+  var imagen=0;
 
-  var colMatch1 = "";
-  var colMatch2 = "";
-  var colMatch3 = "";
-  var aux = "";
-
-  for (var i = 1; i < 8; i++) {
-
-    try {
-      //Se recorren las columnas
-      for (var r = 0; r < 7; r++) {
-        aux = listDelete.length;
-        colMatch1=$(".panel-tablero .col-"+(i)).children().children().get(+(r)).attributes[1]
-        colMatch2=$(".panel-tablero .col-"+(i)).children().children().get(+(r+1)).attributes[1]
-        colMatch3=$(".panel-tablero .col-"+(i)).children().children().get(+(r+2)).attributes[1]
-
-        //var colMatch3=$(".col-"+(r+2)).children("img:nth-child("+i+")").attr("src")
-
-        if (colMatch1 != null && colMatch2 != null && colMatch3 != null){
-          if (colMatch1.value == colMatch2.value && colMatch2.value == colMatch3.value){
-            colMatch1=$(".panel-tablero .col-"+(i)).children().children().get(r);
-            colMatch2=$(".panel-tablero .col-"+(i)).children().children().get(r+1);
-            colMatch3=$(".panel-tablero .col-"+(i)).children().children().get(r+2);
-
-            try {
-              console.log("Si hay coincidencias de columna " + i + " Fila " + r);
-                listDelete[aux] = $(".panel-tablero .col-"+(i)).children().get(r).firstElementChild;
-                listDelete[aux+1] = $(".panel-tablero .col-"+(i)).children().get(r+1).firstElementChild;
-                listDelete[aux+2] = $(".panel-tablero .col-"+(i)).children().get(r+2).firstElementChild;
-
-
-
-            } catch (e) {
-              console.log("No se puede agregar a la lista el elemento a eliminar: " + e.message);
-            }
-          }
-        }
+  $(".elemento").draggable({ disabled: true });
+  if(i<8)
+  {
+    for(var l=1;l<8;l++)
+    {
+      if($(".col-"+l).children("img:nth-child("+i+")").html()==null)
+      {
+        numero=Math.floor(Math.random() * 4) + 1 ;
+        imagen="image/"+numero+".png";
+        $(".col-"+l).prepend("<img src="+imagen+" class='elemento'/>")//.css("justify-content","flex-start")
       }
-    } catch (e) {
-      console.log("Error al ubicar una columna: " + e.message);
-    }finally{
-      listDelete = listDelete.filter(function(listDelete){
-        return listDelete != undefined;
-        console.log("Lista de findVertical");
-        console.log(listDelete);
-      });
     }
   }
-
-  return listDelete;
-
+  if(i==8)
+  {
+    clearInterval(intervalo);   //desactivar funcion desplazamiento()
+    eliminar=setInterval(function(){eliminarhorver()},150)  //activar funcion eliminarhorver
+  }
 }
+//------------------------------------------------------------------------------
+//---------------Funcion para eliminar mas de 3 dulces--------------------------
+function eliminarhorver()
+{
+  arrayCandies=0;
+  rbh=horizontal()  //funcion busqueda dulces horizontal
+  rbv=vertical()    //funcion buscar dulces vertical
 
-function findHorizontal(listDelete){
-
-  var rowMatch1 = "";
-  var rowMatch2 = "";
-  var rowMatch3 = "";
-
-  var aux = "";
-
-  for (var i = 1; i < 8; i++) {
-
-    try {
-      //Se recorren las columnas
-      for (var l = 0; l < 7; l++) {
-        /*if (listDelete.length ==0) {
-          aux = listDelete.length;
-        }else {
-          aux = listDelete.length + 1;
-        }*/
-
-        aux = listDelete.length;
-
-        rowMatch1=$(".panel-tablero .col-"+(i)).children().children().get(+(l)).attributes[1]
-        rowMatch2=$(".panel-tablero .col-"+(i+1)).children().children().get(+(l)).attributes[1]
-        rowMatch3=$(".panel-tablero .col-"+(i+2)).children().children().get(+(l)).attributes[1]
-
-        if (rowMatch1 != null && rowMatch2 != null && rowMatch3 != null){
-          if (rowMatch1.value == rowMatch2.value && rowMatch2.value == rowMatch3.value){
-            rowMatch1=$(".panel-tablero .col-"+(i)).children().children().get(+(l));
-            rowMatch2=$(".panel-tablero .col-"+(i+1)).children().children().get(+(l));
-            rowMatch3=$(".panel-tablero .col-"+(i+2)).children().children().get(+(l));
-
-            try {
-                listDelete[aux] = $(".panel-tablero .col-"+(i)).children().get(+(l)).firstElementChild;
-                listDelete[aux+1] = $(".panel-tablero .col-"+(i+1)).children().get(+(l)).firstElementChild;
-                listDelete[aux+2] = $(".panel-tablero .col-"+(i+2)).children().get(+(l)).firstElementChild;
-
-            } catch (e) {
-              console.log("No se puede agregar a la lista el elemento a eliminar: " + e.message);
-            }finally{
-              listDelete = listDelete.filter(function(listDelete){
-                animateToDelete(listDelete);
-                return listDelete != undefined;
-              });
-
-            }
-          }
-        }
-      }
-    } catch (e) {
-      console.log("Error al ubicar una columnas: " + e.message);
-    }finally{
-console.log("Posición: " + "1");
-      listDelete = listDelete.filter(function(listDelete){
-        return listDelete != undefined;
-
-      });
-    }
-
-    animateToDelete(listDelete);
+  for(var j=1;j<8;j++)
+  {
+      arrayCandies=arrayCandies+$(".col-"+j).children().length;
   }
 
-  function animateToDelete(listDelete){
+  if(rbh==0 && rbv==0 && arrayCandies!=49)  //condicion si no encuentra 3 dulces o mas llamar a funcion para volver a completar el uego
+  {
+      clearInterval(eliminar);
+      bnewd=0;
+      newdulces=setInterval(function()
+      {
+        nuevosdulces()  //Funcion completar nuevos dulces
+      },600)
+  }
+  if(rbh==1 || rbv==1)
+  {
+    $(".elemento").draggable({ disabled: true });
+    $("div[class^='col']").css("justify-content","flex-end")
+    $(".activo").hide("pulsate",1000,function(){
+      var scoretmp=$(".activo").length;
+      $(".activo").remove("img")
+      score=score+scoretmp;
+      $("#score-text").html(score)  //Cambiar puntuacion
+    })
+  }
 
-var element = ""
-    for (var i = 0; i < listDelete.length; i++) {
-console.log(listDelete[i].attributes[0]);
-      element =listDelete[i].attributes[0];
-      console.log("Elemento");
-      console.log(element.value);
-      //$('#"+ element.value +"').fadeIn('fast').delay(5000).fadeOut('fast')
-      $("#" +element.value+ "").fadeIn('fast').delay(10).fadeOut('fast')
-    }
-}
-/*
-  // TODO: TERMINAR EL EFECTO!!!!
-  function animateToDelete(listDelete){
-    for (var i = 0; i < listDelete.length; i++) {
-      parpadear();
-    }
-    function parpadear(){
-      //console.log(listDelete[i]);
-      //$(".row-1").get(0).children[0].fadeIn(500).delay(250).fadeOut(500, parpadear)
-      //listDelete[i].fadeIn(500).delay(250).fadeOut(500, parpadear)
-      a.toggle("fade")
-      //listDelete[i].fadeIn(500,function(){
-        //listDelete[i].fadeOut(500);
-      //})
-    }
-    setInterval(deleteCandies(listDelete),100);
-  }*/
+  if(rbh==0 && rbv==0 && arrayCandies==49)
+  {
+    $(".elemento").draggable({
+      disabled: false,
+      containment: ".panel-tablero",
+      revert: true,
+      revertDuration: 0,
+      snap: ".elemento",
+      snapMode: "inner",
+      snapTolerance: 40,
+      start: function(event, ui){
+        mov=mov+1;
+        $("#movimientos-text").html(mov)
+      }
+    });
+  }
 
-return
-}
-
-// NOTE: ***********************************************************
-function deleteCandies(listDelete){
-
-  listDelete.filter(function(listDelete){
-    return listDelete !== undefined
-    console.log(listDelete);
+  $(".elemento").droppable({
+    drop: function (event, ui) {
+      var dropped = ui.draggable;
+      var droppedOn = this;
+      espera=0;
+      do{
+        espera=dropped.swap(droppedOn);
+      }while(espera==0)
+      rbh=horizontal()  //funcion busqueda dulces horizontal
+      rbv=vertical()    //funcion buscar dulces vertical
+      if(rbh==0 && rbv==0)
+      {
+        dropped.swap(droppedOn);
+      }
+      if(rbh==1 || rbv==1)
+      {
+        clearInterval(newdulces);
+        clearInterval(eliminar);   //desactivar funcion desplazamiento()
+        eliminar=setInterval(function(){eliminarhorver()},150)  //activar funcion eliminarhorver
+      }
+    },
   });
+}
+//------------------------------------------------------------------------------
+//---------Funcion para intercambiar dulces-------------------------------------
+jQuery.fn.swap = function(b)
+{
+    b = jQuery(b)[0];
+    var a = this[0];
+    var t = a.parentNode.insertBefore(document.createTextNode(''), a);
+    b.parentNode.insertBefore(a, b);
+    t.parentNode.insertBefore(b, t);
+    t.parentNode.removeChild(t);
+    return this;
+};
+//------------------------------------------------------------------------------
 
-  for (var i = 0; i < listDelete.length; i++) {
 
-    console.log("Posición: " + i);
-    console.log(listDelete[i]);
-    listDelete[i].remove();
-
+//---------Funcion de nuevos dulces---------------------------------------------
+function nuevosdulces()
+{
+  $(".elemento").draggable({ disabled: true });
+  //alert("pase")
+  $("div[class^='col']").css("justify-content","flex-start")
+  for(var j=1;j<8;j++)
+  {
+      lencol[j-1]=$(".col-"+j).children().length;
   }
+  if(bnewd==0)
+  {
+    for(var j=0;j<7;j++)
+    {
+      lenres[j]=(7-lencol[j]);
+    }
+    maximo=Math.max.apply(null,lenres);
+    contador=maximo;
+  }
+  if(maximo!=0)
+  {
+    if(bnewd==1)
+    {
+      for(var j=1;j<8;j++)
+      {
+        if(contador>(maximo-lenres[j-1]))
+        {
+          $(".col-"+j).children("img:nth-child("+(lenres[j-1])+")").remove("img")
+        }
+      }
+    }
+    if(bnewd==0)
+    {
+      bnewd=1;
+      for(var k=1;k<8;k++)
+      {
+        for(var j=0;j<(lenres[k-1]-1);j++)
+        {
+            $(".col-"+k).prepend("<img src='' class='elemento' style='visibility:hidden'/>")
+        }
+      }
+    }
+    for(var j=1;j<8;j++)
+    {
+      if(contador>(maximo-lenres[j-1]))
+      {
+        numero=Math.floor(Math.random() * 4) + 1 ;
+        imagen="image/"+numero+".png";
+        $(".col-"+j).prepend("<img src="+imagen+" class='elemento'/>")
+      }
+    }
+  }
+  if(contador==1)
+  {
+      clearInterval(newdulces);
+      eliminar=setInterval(function(){eliminarhorver()},150)
+  }
+  contador=contador-1;
+}
+//------------------------------------------------------------------------------
+//----------funcion de busqueda horizontal de dulces----------------------------
+function horizontal()
+{
+  var bh=0;
+  for(var j=1;j<8;j++)
+  {
+    for(var k=1;k<6;k++)
+    {
+      var res1=$(".col-"+k).children("img:nth-last-child("+j+")").attr("src")
+      var res2=$(".col-"+(k+1)).children("img:nth-last-child("+j+")").attr("src")
+      var res3=$(".col-"+(k+2)).children("img:nth-last-child("+j+")").attr("src")
+      if((res1==res2) && (res2==res3) && (res1!=null) && (res2!=null) && (res3!=null))
+      {
+          $(".col-"+k).children("img:nth-last-child("+(j)+")").attr("class","elemento activo")
+          $(".col-"+(k+1)).children("img:nth-last-child("+(j)+")").attr("class","elemento activo")
+          $(".col-"+(k+2)).children("img:nth-last-child("+(j)+")").attr("class","elemento activo")
+          bh=1;
+      }
+    }
+  }
+  return bh;
+}
+//------------------------------------------------------------------------------
+//----------Funcion de busqueda vertical de dulces------------------------------
+function vertical()
+{
+  var bv=0;
+  for(var l=1;l<6;l++)
+  {
+    for(var k=1;k<8;k++)
+    {
+      var res1=$(".col-"+k).children("img:nth-child("+l+")").attr("src")
+      var res2=$(".col-"+k).children("img:nth-child("+(l+1)+")").attr("src")
+      var res3=$(".col-"+k).children("img:nth-child("+(l+2)+")").attr("src")
+      if((res1==res2) && (res2==res3) && (res1!=null) && (res2!=null) && (res3!=null))
+      {
+          $(".col-"+k).children("img:nth-child("+(l)+")").attr("class","elemento activo")
+          $(".col-"+k).children("img:nth-child("+(l+1)+")").attr("class","elemento activo")
+          $(".col-"+k).children("img:nth-child("+(l+2)+")").attr("class","elemento activo")
+          bv=1;
+      }
+    }
+  }
+  return bv;
 }
